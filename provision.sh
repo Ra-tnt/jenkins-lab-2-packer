f44af052e7ca512efa0455f80766feec189613c1
Ra-tnt <roody.tnt@gmail.com>
2020-08-13 10:59:43 +0300

    updated

--- provision.sh
+++ provision.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo service nginx start
 echo "done."
